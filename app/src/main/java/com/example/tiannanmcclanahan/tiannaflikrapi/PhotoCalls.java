04678432de90297e198cfb9378729a0738ea1e66
Tianna N. McClanahan <tiannan.mcclanahan@Tiannas-MacBook-Pro.local>
2016-05-20 10:59:22 -0700

    Adds files

---
+++ app/src/main/java/com/example/tiannanmcclanahan/tiannaflikrapi/PhotoCalls.java
@@ -0,0 +1,8 @@
+package com.example.tiannanmcclanahan.tiannaflikrapi;
+
+/**
+ * Created by tiannan.mcclanahan on 5/20/16.
+ */
+public class PhotoCalls {
+    private static PhotoCalls instance;
+}
